a7ff8a90658169254cff98890d86b0200949ce5a
AfaqShahidKhan <afaqk3203@gmail.com>
2024-08-05 22:42:19 +0500

    added map to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -9,6 +9,8 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
@@ -41,14 +43,15 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to='cities' />}
           />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City/>}/>
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>form</p>} />
+          <Route path="form" element={<Form/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 export default function Map() {
+  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   return (
-    <div className={styles.mapContainer}>
-
-
-        map
+    <div className={styles.mapContainer} onClick={()=>navigate('form')}>
+        <h1>positioin {lat}, {lng}</h1>
     </div>
   )
 }
