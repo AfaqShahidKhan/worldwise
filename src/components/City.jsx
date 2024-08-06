64708d40e1a275318012d972168540a2269a3884
AfaqShahidKhan <afaqk3203@gmail.com>
2024-08-06 15:20:41 +0500

    added react leaflet into project

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,9 @@
 import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,20 +15,17 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { getCity, currentCity, isLoading } = useCities();
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
 
   const { cityName, emoji, date, notes } = currentCity;
-
+  if (isLoading) return <Spinner />;
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -57,12 +58,7 @@ function City() {
         </a>
       </div>
 
-      <div>
-        {/* Assuming you have a ButtonBack component */}
-        {/* <ButtonBack /> */}
-      </div>
-      <h1>City {id}</h1>
-      <p>Position: {lat}, {lng}</p>
+      <div><BackButton /></div>
     </div>
   );
 }
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -16,4 +16,3 @@ export default function CityList() {
     </ul>
   );
 }
-// start from  229
