41bc810a8183e477223400a9cd9c43556847b1ed
AfaqShahidKhan <afaqk3203@gmail.com>
2024-08-18 23:16:41 +0500

    add lazy loading to page rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
-import CityList from "./components/CityList";
-import CountryList from "./components/CountryList";
-import City from "./components/City";
-import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
-
+import { lazy, Suspense } from "react";
+import City from "./components/City";
+import Form from "./components/Form";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+const Homepage = lazy(()=>import('./pages/Homepage'))
+const Product = lazy(()=>import('./pages/Product'))
+const Pricing = lazy(()=>import('./pages/Pricing'))
+const PageNotFound = lazy(()=>import('./pages/PageNotFound'))
+const AppLayout = lazy(()=>import('./pages/AppLayout'))
+const Login = lazy(()=>import('./pages/Login'))
+const ProtectedRoute = lazy(()=>import('./pages/ProtectedRoute'))
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage/>}>
           <Routes>
             <Route index element={<Homepage />} />
             <Route path="product" element={<Product />} />
@@ -40,6 +42,7 @@ function App() {
               <Route path="form" element={<Form />} />
             </Route>
           </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
